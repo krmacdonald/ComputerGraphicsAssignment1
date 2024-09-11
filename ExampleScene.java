153d164fd2d7308721376f1a8fdd7b23fd9580fc
KMacdonald <kylemacdo01@gmail.com>
2024-09-11 14:09:18 -0400

    Trees

--- ExampleScene.java
+++ ExampleScene.java
@@ -63,26 +63,30 @@ public class ExampleScene extends JPanel{
     }
     
     private void createTrees(Graphics2D graphics2d){
-        createTree(graphics2d, 30, 70);
-        createTree(graphics2d, 70, 70);
+        createTree(graphics2d, 0, 80);
+        createTree(graphics2d, 80, 80);
     }
 
     private void createTree(Graphics2D graphics2d, double x, double y){
         Path2D polyTree = new Path2D.Double();
         polyTree.moveTo(x, y);
-        polyTree.lineTo(x + 30, y);
-        polyTree.lineTo(x + 20, y - 50);
-        polyTree.lineTo(x + 10, y - 50);
+        polyTree.lineTo(x + 20, y);
+        polyTree.lineTo(x + 15, y - 40);
+        polyTree.lineTo(x + 5, y - 40);
         polyTree.lineTo(x, y);
 
-        graphics2d.setPaint(Color.b);
-        //TODO rgb of brown
+        graphics2d.setPaint(new Color(128, 89, 0));
+
         graphics2d.fill(polyTree);
 
         //creates the leaves using two quadratic curves, need to troubleshoot curvature
         Path2D polyLeaves = new Path2D.Double();
-        polyLeaves.moveTo(x - 10, y - 60);
-        polyLeaves.quadTo(x + 15, y - 40, x + 40, y - 60);
+        polyLeaves.moveTo(x, y - 40);
+        polyLeaves.quadTo(x + 10, y - 10, x + 20, y - 40);
+        polyLeaves.quadTo(x + 10, y - 70, x, y - 40);
+        graphics2d.setPaint(new Color(9, 160, 1));
+
+        graphics2d.fill(polyLeaves);
     }
 
     private void applyWindowToViewportTransformation(Graphics2D graphics2d, double left, double right, double top, double bottom, boolean preserveAspect){
